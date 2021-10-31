8d30650ecd1be2c1accd2b2e6ed6d8f089335fe6
danearboleda <nataarboleda2001@gmail.com>
2021-10-31 18:56:50 -0500

    Se agregan los elementos del MS de ventas

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,1 +1,6 @@
+export * from './factura-venta.controller';
 export * from './ping.controller';
+export * from './venta-factura.controller';
+
+export * from './venta.controller';
+export * from './factura.controller';
